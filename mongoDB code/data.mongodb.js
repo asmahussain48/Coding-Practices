818900ab7e6dd108d36eba833d279ffb07db7e7c
asmahussain48 <asma.hussain.mughal@gmail.com>
2026-03-28 14:22:50 +0500

    init

--- mongoDB code/data.mongodb.js
+++ mongoDB code/data.mongodb.js
@@ -32,7 +32,6 @@ db.products.insertMany([
   }
 ])
 
-
 db.contacts.insertMany([
   { name: "Alice", message: "Loved your website!", phone: "9876543210", createdAt: new Date() },
   { name: "Bob", message: "Do you have discounts on laptops?", phone: "9123456789", createdAt: new Date() },
---
+++ mongoDB code/reading.mongodb.js
@@ -0,0 +1,3 @@
+use("ecommerce");
+db.products.find({"name": "Gaming Laptop"})
+db.products.find({category: "Computers"})
